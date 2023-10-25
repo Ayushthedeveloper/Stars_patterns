bcee82aaa4016f5aa97a3ea5a2bcb4f8ed07ca2f
Ayush <artisri1984@gmail.com>
2023-10-25 15:05:39 +0530

    BUTTERFLY PATTERN

--- star patterns.py
+++ star patterns.py
@@ -90,3 +90,29 @@ for i in range(n):
     for j in range(i,n):
         print("*", end=" ")
     print()
+
+# BUTTTERFLY PATTERN
+
+n = 4
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    for j in range(i,n-1):
+        print(" ", end=" ")
+    for j in range(i,n):
+        print(" ", end= " ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+
+for i in range(n):
+    for j in range(i, n):
+        print("*", end=" ")
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(i+1):
+        print(" ", end=" ")
+    for j in range(i, n):
+        print("*", end=" ")
+    print()
+
